e9b446b3373ce715deb5850471b32d197e09908a
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-04-17 17:31:45 +0900

    add read csv & simple count wins

--- build/index.js
+++ build/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log("test");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+// read csv
+var matches = fs_1.default
+    .readFileSync("original.csv", {
+    encoding: "utf-8",
+})
+    .split("\n")
+    .map(function (match) {
+    return match.split(",");
+});
+// count manUnited wins
+var manUnitedWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === "Man United" && match[5] === "H")
+        manUnitedWins++;
+    if (match[2] === "Man United" && match[5] === "A")
+        manUnitedWins++;
+}
+console.log(manUnitedWins);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("test");
+import fs from "fs";
+
+// read csv
+const matches = fs
+  .readFileSync("original.csv", {
+    encoding: "utf-8",
+  })
+  .split("\n")
+  .map((match) => {
+    return match.split(",");
+  });
+
+// count manUnited wins
+let manUnitedWins = 0;
+for (const match of matches) {
+  if (match[1] === "Man United" && match[5] === "H") manUnitedWins++;
+  if (match[2] === "Man United" && match[5] === "A") manUnitedWins++;
+}
+
+console.log(manUnitedWins);
