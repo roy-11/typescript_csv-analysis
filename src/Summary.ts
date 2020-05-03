81f842d7328150930703257b04b73b988b7d6332
ryosukehagiwara <hukurahagi1109@gmail.com>
2020-05-04 00:05:40 +0900

    add build and printReport method into Summary class

--- src/Summary.ts
+++ src/Summary.ts
@@ -10,4 +10,9 @@ export interface OutputTarget {
 
 export class Summary {
   constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {}
+
+  buildAndPrintReport(matches: MatchData[]) {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
